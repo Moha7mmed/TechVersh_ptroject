dbaa6330a46713ff257cb84ddb04a8c613fc3a1d
Mohammed AL darora <maldrorh44@gmail.com>
2024-02-10 20:42:51 +0300

    أول تعليق

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,12 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last, prefer_const_literals_to_create_immutables
+/*
+ملاحظة
+من ناحية الالوان ما طلعت النتيجة الي ترضيني(اهم شي يعني اخلص المشروع)
+وايضًا عارف 
+أن اقدر اخلي عدد الاكواد اقل من كذا لكن يبغا ليي اتعلم على هذا الشي
+وبس
+
+ */
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,121 +14,233 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Color.fromARGB(0xFF, 0x7C, 0x85, 0x8D),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(0xFF, 0x49, 0x50, 0x57),
+          title: Text(
+            "ملف شخصي",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Column(children: [
+          Center(
+            child: CircleAvatar(
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("img/112233.webp"),
+              radius: 100,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: 380,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6),
+                      size: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "الاسم",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        ),
+                        Text(
+                          "محمد",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.arrow_forward_sharp,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(0xFF, 0x49, 0x50, 0x57),
+                borderRadius: BorderRadius.circular(15)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 380,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6),
+                      size: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "رقم الهاتف",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        ),
+                        Text(
+                          "051 085 ****",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.arrow_forward_sharp,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(0xFF, 0x49, 0x50, 0x57),
+                borderRadius: BorderRadius.circular(15)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 380,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.location_pin,
+                      color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6),
+                      size: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "العنوان",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        ),
+                        Text(
+                          "****",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.arrow_forward_sharp,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(0xFF, 0x49, 0x50, 0x57),
+                borderRadius: BorderRadius.circular(15)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 380,
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.email_rounded,
+                      color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6),
+                      size: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "الإيميل",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        ),
+                        Text(
+                          "******@gmail.com",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(0xFF, 0xDE, 0xE2, 0xE6)),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.arrow_forward_sharp,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(0xFF, 0x49, 0x50, 0x57),
+                borderRadius: BorderRadius.circular(15)),
+          ),
+        ]));
   }
 }
